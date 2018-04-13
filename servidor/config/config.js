87d0019f79a896c96dd77ebc357405e9d37d7bb7
gamaliel <fabiogamaliel@gmail.com>
2018-04-13 05:10:27 -0400

    config created

---
+++ servidor/config/config.js
@@ -0,0 +1,12 @@
+export default {
+	server: {
+		host: 'localhost',
+		port: 3000
+	},
+	database: {
+		host: 'localhost',
+		post: 27017,
+		db: 'agenda',
+		url: 'mongodb://127.0.0.1:27017/agenda'
+	}
+}
