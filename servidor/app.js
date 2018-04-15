016c65507e87a38f1c54b372188d950267cb3d86
gamaliel <fabiogamaliel@gmail.com>
2018-04-15 04:29:06 -0400

    Access-Control added

--- servidor/app.js
+++ servidor/app.js
@@ -10,12 +10,20 @@ import db from './config/db'
 
 let app = express()
 
-require('./app/routes')(app)
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080")
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials")
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
+
+require('./app/routes')(app)
 
 app.listen(3000, function (err) {
 	if(err) {
