370198407ce8658ab565dbd8caedefdbe1ac01be
gamaliel <fabiogamaliel@gmail.com>
2018-04-15 04:29:41 -0400

    routes created

--- servidor/app/agenda/agenda.routes.js
+++ servidor/app/agenda/agenda.routes.js
@@ -3,4 +3,17 @@ import agendaCtrl from './agenda.controller.js'
 export default (app) => {
 	app.route('/api/v1/anotaciones')
 	   .get(agendaCtrl.listar)
+
+	app.route('/api/v1/anotaciones/nuevo')
+	   .post(agendaCtrl.crear)
+
+	app.route('/api/v1/anotaciones/:idAnotacion/mostrar')
+	   .get(agendaCtrl.mostrar)
+
+	app.route('/api/v1/anotaciones/:idAnotacion/eliminar')
+	   .delete(agendaCtrl.eliminar)
+
+	app.route('/api/v1/anotaciones/:idAnotacion/actualizar')
+	   .put(agendaCtrl.actualizar)
+	   
 }
